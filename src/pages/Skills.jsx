97f1a35e2949b9045655aaa2a63c57804b42947b
Salay Abdul Muhaimin Kanton <abdulkanton2005@gmail.com>
2025-01-20 07:32:26 +0000

    Add Skill Names

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -15,18 +15,23 @@ export default function Skills() {
             <div className="row justify-content-center">
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/python.svg" alt="Python" className="img-fluid skill-icon" />
+                <span>Python</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/html.svg" alt="HTML" className="img-fluid skill-icon" />
+                <span>HTML 5</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/css.svg" alt="CSS" className="img-fluid skill-icon" />
+                <span>CSS 3</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/javascript.svg" alt="JavaScript" className="img-fluid skill-icon" />
+                <span>JavaScript</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/typescript.svg" alt="Typescript" className="img-fluid skill-icon" />
+                <span>Typescript</span>
               </div>
             </div>
           </div>
@@ -35,26 +40,36 @@ export default function Skills() {
             <div className="row justify-content-center">
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/reactjs.svg" alt="React" className="img-fluid skill-icon" />
+                <span>React</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/bootstrap.svg" alt="Bootstrap" className="img-fluid skill-icon" />
+                <span>Bootstrap</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/tailwind.svg" alt="Tailwind" className="img-fluid skill-icon" />
+                <span>Tailwindcss</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/postcss.svg" alt="PostCSS" className="img-fluid skill-icon" />
+                <span>Postcss</span>
               </div>
             </div>
           </div>
           <div className="section-body" data-aos="fade-up" data-aos-delay="100">
-            <h4>Mobile Frameworks</h4>
+            <h4>Mobile & Desktop Frameworks</h4>
             <div className="row justify-content-center">
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/expo.svg" alt="Expo" className="img-fluid skill-icon" />
+                <span>Expo</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/react-native.svg" alt="React Native" className="img-fluid skill-icon" />
+                <span>React Native</span>
+              </div>
+              <div className="col-6 col-sm-4 col-md-2">
+                <img src="./skills/tkinter.svg" alt="Tkinter" className="img-fluid skill-icon" />
+                <span>Tkinter</span>
               </div>
             </div>
           </div>
@@ -63,12 +78,15 @@ export default function Skills() {
             <div className="row justify-content-center">
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/django.svg" alt="Django" className="img-fluid skill-icon" />
+                <span>Django</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/flask.jpg" alt="Flask" className="img-fluid skill-icon" />
+                <span>Flask</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/next.svg" alt="Next" className="img-fluid skill-icon" />
+                <span>Next.js</span>
               </div>
             </div>
           </div>
@@ -77,15 +95,19 @@ export default function Skills() {
             <div className="row justify-content-center">
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/git.svg" alt="Git" className="img-fluid skill-icon" />
+                <span>Git</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/npm.svg" alt="NPM" className="img-fluid skill-icon" />
+                <span>NPM</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/pip.svg" alt="PIP" className="img-fluid skill-icon" />
+                <span>PIP</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/vite.svg" alt="Vite" className="img-fluid skill-icon" />
+                <span>Vite</span>
               </div>
             </div>
           </div>
@@ -94,12 +116,15 @@ export default function Skills() {
             <div className="row justify-content-center">
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/firebase.svg" alt="Firebase" className="img-fluid skill-icon" />
+                <span>Firebase</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/netlify.svg" alt="Netlify" className="img-fluid skill-icon" />
+                <span>Netlify</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/vercel.svg" alt="Vercel" className="img-fluid skill-icon" />
+                <span>Vercel</span>
               </div>
             </div>
           </div>
@@ -108,42 +133,51 @@ export default function Skills() {
             <div className="row justify-content-center">
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/arduino.svg" alt="Arduino" className="img-fluid skill-icon" />
+                <span>Arduino</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/nsis.svg" alt="NSIS" className="img-fluid skill-icon" />
+                <span>NSIS</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/innosetup.svg" alt="Innosetup" className="img-fluid skill-icon" />
+                <span>Inno Setup</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/pgsql.svg" alt="PostgreSQL" className="img-fluid skill-icon" />
+                <span>PostgreSQL</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/sqlite.svg" alt="SQLite" className="img-fluid skill-icon" />
+                <span>SQLite</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/vscode.svg" alt="Visual Studio Code" className="img-fluid skill-icon" />
+                <span>Visual Studio Code</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/shell.svg" alt="Shell" className="img-fluid skill-icon" />
+                <span>Shell</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/gimp.svg" alt="GIMP" className="img-fluid skill-icon" />
+                <span>GIMP</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/openshot.svg" alt="OpenShot" className="img-fluid skill-icon" />
+                <span>OpenShot</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/obs-studio.jpg" alt="OBS Studio" className="img-fluid skill-icon" />
+                <span>OBS Studio</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/app-sheet.svg" alt="App Sheet" className="img-fluid skill-icon" />
+                <span>App Sheet</span>
               </div>
               <div className="col-6 col-sm-4 col-md-2">
                 <img src="./skills/github.svg" alt="Github" className="img-fluid skill-icon" />
-              </div>
-              <div className="col-6 col-sm-4 col-md-2">
-                <img src="./skills/tkinter.svg" alt="Tkinter" className="img-fluid skill-icon" />
+                <span>Github</span>
               </div>
             </div>
           </div>
