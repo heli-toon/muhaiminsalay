f3a511086a0ae13f28bb4f0bee6f371b69a05148
Salay Abdul Muhaimin Kanton <abdulkanton2005@gmail.com>
2026-06-09 13:33:27 +0000

    Remove unused Link import from Prompts component

--- src/pages/Prompts.tsx
+++ src/pages/Prompts.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { collection, getDocs } from "firebase/firestore";
 
 import { db } from "../lib/firebase";
